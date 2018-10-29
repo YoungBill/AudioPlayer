054c9209e22a62cdf9890664a7a2186a7babbe25
taochen <taochen@baina.com>
2018-10-29 15:23:27 +0800

    [feature] 优化代码结构和ui细节

--- app/src/main/java/playaudio/android/com/MusicView.java
+++ app/src/main/java/playaudio/android/com/MusicView.java
@@ -25,73 +25,12 @@ public class MusicView extends LinearLayout implements View.OnClickListener {
     private ImageButton mPreviewSwitch;
     private ImageButton mNextSwitch;
     private MusicService mMusicService;
-    private MusicCallback mMusicCallback = new MusicCallback() {
-
-        @Override
-        public void onFinishLoadingMusicList(RadioResponse.DataBean.ItemsBean firstMusicBean) {
-            mFrequencyTv.setText(firstMusicBean.getFrequency());
-            String title = getContext().getString(R.string.audio_title, firstMusicBean.getTitle());
-            mTitleTv.setText(title);
-            mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_playing);
-            mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_play);
-        }
-
-        @Override
-        public void onLoading(RadioResponse.DataBean.ItemsBean musicBean) {
-            mFrequencyTv.setText(musicBean.getFrequency());
-            String title = getContext().getString(R.string.audio_title, musicBean.getTitle());
-            mTitleTv.setText(title);
-            mTitleTv.stopScroll();
-            mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_loading);
-            mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_loading);
-            mPlayOrPauseSwitch.setShowLoading(true);
-        }
-
-        @Override
-        public void onPlay(RadioResponse.DataBean.ItemsBean musicBean) {
-            mTitleTv.resumeScroll();
-            mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_playing);
-            mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_pause);
-            mPlayOrPauseSwitch.setShowLoading(false);
-        }
-
-        @Override
-        public void onPause(RadioResponse.DataBean.ItemsBean musicBean) {
-            mTitleTv.pauseScroll();
-            mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_playing);
-            mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_play);
-            mPlayOrPauseSwitch.setShowLoading(false);
-        }
-
-        @Override
-        public void onError(int what, String message) {
-
-        }
-    };
-    private ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            mMusicService = ((MusicService.ServiceBinder) iBinder).getService();
-
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            mMusicService = null;
-        }
-    };
+    private MusicCallback mMusicCallback;
+    private ServiceConnection mServiceConnection;
 
     public MusicView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        LayoutInflater.from(context).inflate(R.layout.view_music, this);
-        mFrequencyTv = findViewById(R.id.frequencyTv);
-        mTitleTv = findViewById(R.id.titleTv);
-        mPlayOrPauseSwitch = findViewById(R.id.play_or_pause_switch);
-        mPlayOrPauseSwitch.setOnClickListener(this);
-        mPreviewSwitch = findViewById(R.id.preview_switch);
-        mPreviewSwitch.setOnClickListener(this);
-        mNextSwitch = findViewById(R.id.next_switch);
-        mNextSwitch.setOnClickListener(this);
+        init(context);
     }
 
     @Override
@@ -123,5 +62,72 @@ public class MusicView extends LinearLayout implements View.OnClickListener {
                 mMusicService.switchNext();
                 break;
         }
+    }
+
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.view_music, this);
+        mFrequencyTv = findViewById(R.id.frequencyTv);
+        mTitleTv = findViewById(R.id.titleTv);
+        mPlayOrPauseSwitch = findViewById(R.id.play_or_pause_switch);
+        mPlayOrPauseSwitch.setOnClickListener(this);
+        mPreviewSwitch = findViewById(R.id.preview_switch);
+        mPreviewSwitch.setOnClickListener(this);
+        mNextSwitch = findViewById(R.id.next_switch);
+        mNextSwitch.setOnClickListener(this);
+        mMusicCallback = new MusicCallback() {
+
+            @Override
+            public void onFinishLoadingMusicList(RadioResponse.DataBean.ItemsBean firstMusicBean) {
+                mFrequencyTv.setText(firstMusicBean.getFrequency());
+                String title = getContext().getString(R.string.audio_title, firstMusicBean.getTitle());
+                mTitleTv.setText(title);
+                mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_playing);
+                mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_play);
+            }
+
+            @Override
+            public void onLoading(RadioResponse.DataBean.ItemsBean musicBean) {
+                mFrequencyTv.setText(musicBean.getFrequency());
+                String title = getContext().getString(R.string.audio_title, musicBean.getTitle());
+                mTitleTv.setText(title);
+                mTitleTv.stopScroll();
+                mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_loading);
+                mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_loading);
+                mPlayOrPauseSwitch.setShowLoading(true);
+            }
+
+            @Override
+            public void onPlay(RadioResponse.DataBean.ItemsBean musicBean) {
+                mTitleTv.resumeScroll();
+                mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_playing);
+                mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_pause);
+                mPlayOrPauseSwitch.setShowLoading(false);
+            }
+
+            @Override
+            public void onPause(RadioResponse.DataBean.ItemsBean musicBean) {
+                mTitleTv.pauseScroll();
+                mPlayOrPauseSwitch.setOuterImageResource(R.mipmap.play_status_playing);
+                mPlayOrPauseSwitch.setInnerImageResource(R.mipmap.play_btn_play);
+                mPlayOrPauseSwitch.setShowLoading(false);
+            }
+
+            @Override
+            public void onError(int what, String message) {
+
+            }
+        };
+        mServiceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                mMusicService = ((MusicService.ServiceBinder) iBinder).getService();
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                mMusicService = null;
+            }
+        };
     }
 }
